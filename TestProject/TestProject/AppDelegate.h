b2bb28ffb9ea9a5ccbbe8642861409eff1369653
yxg <xingui.yan@163.com>
2017-08-21 09:07:22 +0800

    first commit

---
+++ TestProject/TestProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProject
+//
+//  Created by yxg on 2017/8/21.
+//  Copyright © 2017年 Ving. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
